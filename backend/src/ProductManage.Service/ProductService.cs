ae129f4c4a1c548a66a9e2e3708aca374841bdf2
Emilio Veras <emilio_ivanovich@hotmail.com>
2025-10-12 20:57:46 -0400

    [ADD] Service Product Class implementing the IService Interface

--- backend/src/ProductManage.Service/ProductService.cs
+++ backend/src/ProductManage.Service/ProductService.cs
@@ -1,6 +1,47 @@
-﻿namespace ProductManage.Service;
+﻿using ProductManager.Domain.Contracts;
+using ProductManager.Domain.Models;
 
-public class ProductService
+namespace ProductManage.Service;
+
+public class ProductService : IService
 {
+    private readonly IRepository<ProductModel> _repository;
 
+    public ProductService(IRepository<ProductModel> repository)
+    {
+        _repository = repository;
+    }
+
+    public Task<ProductModel> Add(ProductModel entity)
+    {
+        return _repository.Add(entity);
+    }
+
+    public Task<ProductModel> FirstOrDefault(string searchFilter)
+    {
+        return _repository.FirstOrDefault(e =>
+            e.Name.Contains(searchFilter) ||
+            e.Category.Contains(searchFilter) ||
+            e.Price.ToString().Contains(searchFilter) ||
+            e.Stock.ToString().Contains(searchFilter));
+    }
+
+    public Task<PaginatedListModel<ProductModel>> GetPaginated<TField>(int pageIndex, int pageSize, string searchFilter)
+    {
+        return _repository.GetPaginated<TField>(pageIndex, pageSize, e =>
+            e.Name.Contains(searchFilter) ||
+            e.Category.Contains(searchFilter) ||
+            e.Price.ToString().Contains(searchFilter) ||
+            e.Stock.ToString().Contains(searchFilter));
+    }
+
+    public Task<ProductModel> Remove(ProductModel entity)
+    {
+        return _repository.Remove(entity);
+    }
+
+    public Task<ProductModel> Update(ProductModel entity)
+    {
+        return _repository.Update(entity);
+    }
 }
---
+++ backend/src/ProductManager.Domain/Contracts/IService.cs
@@ -0,0 +1,12 @@
+﻿using ProductManager.Domain.Models;
+
+namespace ProductManager.Domain.Contracts;
+
+public interface IService
+{
+    public Task<ProductModel> Add(ProductModel entity);
+    public Task<ProductModel> Update(ProductModel entity);
+    public Task<ProductModel> Remove(ProductModel entity);
+    public Task<ProductModel> FirstOrDefault(string searchString);
+    public Task<PaginatedListModel<ProductModel>> GetPaginated<TField>(int pageIndex, int pageSize, string searchFilter);
+}
